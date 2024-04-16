c7b8ee0bc1a68162d2281e5424547bbc89599fa1
flatypus [mac] <yhc3141@gmail.com>
2024-04-16 16:49:43 -0700

    initial commit

---
+++ DiscordRichPresence/Program.cs
@@ -0,0 +1,100 @@
+﻿using Discord;
+using System.Text.Json;
+
+class Config
+{
+    public required long clientID { get; set; }
+    public required string state { get; set; }
+    public string? details { get; set; }
+    public long? startTimestamp { get; set; }
+    public string? largeImage { get; set; }
+    public string? largeText { get; set; }
+    public string? smallImage { get; set; }
+    public string? smallText { get; set; }
+}
+
+class ConfigReader
+{
+    public Config config;
+    public Config readConfig()
+    {
+        string text = File.ReadAllText(@"./config.json");
+        var configResult = JsonSerializer.Deserialize<Config>(text);
+        if (configResult == null)
+        {
+            throw new Exception("Failed to parse config.json");
+        }
+        config = configResult;
+        return configResult;
+    }
+
+    public ConfigReader()
+    {
+        readConfig();
+    }
+}
+
+
+class DiscordActivityUpdater
+{
+    private ConfigReader configReader;
+    private static ActivityManager activityManager;
+
+    public DiscordActivityUpdater()
+    {
+        configReader = new ConfigReader();
+        Console.WriteLine("Starting Discord Activity Updater");
+        var discord = new Discord.Discord(configReader.config.clientID, (ulong)CreateFlags.NoRequireDiscord);
+        activityManager = discord.GetActivityManager();
+
+        SetActivity();
+        while (true)
+        {
+            SetActivity();
+            Task.Delay(1000).Wait();
+        }
+    }
+
+    void SetActivity()
+    {
+        var config = configReader.readConfig();
+        Console.WriteLine("Setting activity");
+        Console.WriteLine(config.state);
+        var startTimestamp = DateTimeOffset.Now.ToUnixTimeSeconds();
+
+        var activity = new Activity
+        {
+            State = config.state,
+            Details = config.details ?? "",
+            Timestamps = { Start = config.startTimestamp ?? startTimestamp },
+            Assets =
+            {
+                LargeImage = config.largeImage ?? "",
+                LargeText = config.largeText ?? "",
+                SmallImage = config.smallImage ?? "",
+                SmallText = config.smallText ?? "",
+            },
+            Instance = true,
+        };
+
+        activityManager.UpdateActivity(activity, (result) =>
+        {
+            if (result == Result.Ok)
+            {
+                Console.WriteLine("Success!");
+            }
+            else
+            {
+                Console.WriteLine("Failed");
+            }
+        });
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        DiscordActivityUpdater discordActivityUpdater = new DiscordActivityUpdater();
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Discord Rich Presence with the Discord Game SDK
+
+This is a simple example of connecting to the Discord RPC with C# through a console app. To use this, copy the config.json.example in /DiscordRichPresence/config.json.example to /DiscordRichPresence/config.json and fill in the client ID of your application.
